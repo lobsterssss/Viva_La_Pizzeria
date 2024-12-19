f361b94faffe4340a25d4dff26622f4c2d19f365
Lobster <92152762+lobsterssss@users.noreply.github.com>
2024-12-19 10:43:41 +0100

    fix crash on unused productID

--- app/Livewire/BesteldePizza.php
+++ app/Livewire/BesteldePizza.php
@@ -8,19 +8,22 @@ use Livewire\Component;
 class BesteldePizza extends Component
 {
     public $product;
-    public $ProductID;
     
     public $amount = 1;
 
     public function mount(Pizza_bestelling $product) {
         $this->product = $product;
+
         $this->amount = $product->Aantal;
     }
     public function removeIncrement() {
         $this->product->amount = $this->product->amount--;
+        return $this->product;
     }
     public function addIncrement() {
         $this->product->Aantal++;
+        return $this->product;
+
     }
 
     public function render()
